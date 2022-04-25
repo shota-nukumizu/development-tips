c05570bb4018b301ae950a35ed472503ef44aaf6
shota-nukumizu <author2000.1225@gmail.com>
2022-04-25 15:27:44 +0900

    modified decorator-ts

--- typescript/src/decorator.ts
+++ typescript/src/decorator.ts
@@ -1,10 +1,25 @@
-function logged(constructorFn: Function) {
-    console.log(constructorFn)
+/* 
+デコレータとは、クラス宣言、メソッド、アクセサ、プロパティやパラメータに 
+付加できる特殊な宣言。
+実行時に装飾された宣言の情報とともに呼び出さなければならない
+*/
+
+function first() {
+    console.log('first(): factory evaluated')
+    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+        console.log('first(): called')
+    }
 }
 
-@logged
-class Person {
-    constructor() {
-        console.log('Hello TypeScript')
+function second() {
+    console.log('second(): factory evaluated')
+    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+        console.log('second(): called')
     }
+}
+
+class ExampleClass {
+    @first()
+    @second()
+    method() { console.log('METHOD') }
 }
